4ba2c69bbb66ad59739555a886f1291af6babe25
Popa Marius Adrian <mapopa@gmail.com>
2012-03-29 16:59:44 +0300

    add Micropost resource see 2.3.1 subchapter

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120327192907) do
+ActiveRecord::Schema.define(:version => 20120328132538) do
 
   create_table "users", :force => true do |t|
     t.string   "name",       :scale => 0
